6644c0901e7cf3b84e9ee41bae1e115fa0f2ad8f
Pablo Bellizzi <pbellizzi2@gmail.com>
2023-02-28 20:59:15 -0300

    separate files

---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.querySelector("audio");
+    let track = document.querySelector("#track");
+    let images = [...document.querySelectorAll('.image')]
+
+    let mouseDownAt = 0; 
+    let movedPercentage = 0; 
+    let prevPercentage = 0;
+
+    window.addEventListener("mousedown" ,(e) => handleOnDown(e));
+    window.addEventListener("mousemove" ,(e) => handleOnMove(e));
+    window.addEventListener("mouseup"   ,(e) => handleOnUp(e)  );
+
+    /*window.ontouchstart = e => handleOnDown(e.touches[0]);
+    window.ontouchend = e => handleOnUp(e.touches[0]);
+    window.ontouchmove = e => handleOnMove(e.touches[0]);*/
+
+    for (image of images){
+        let elRect    =   image.getBoundingClientRect();
+        let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
+        let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
+        /*image.style.objectPosition = `${posicion}% center`*/
+        image.setAttribute = ("objectPosition",`${posicion}% center`)
+                        }
+                    
+
+    const handleOnDown = e => {
+        mouseDownAt = parseInt(e.clientX)
+        }
+
+    const handleOnUp = () => {
+        /*audio.pause();*/
+            mouseDownAt = 0;
+            movedPercentage = prevPercentage
+        }
+    
+ 
+    const handleOnMove = e => {
+         if((mouseDownAt) === 0) return
+         /*audio.play()*/
+
+        const mouseDelta = parseFloat(mouseDownAt) - e.clientX; 
+    
+        /* No estoy usando el ancho de la pantalla para determinar la velocidad del scroll*/
+        /*const   maxDelta = window.innerWidth / 2;*/ 
+        
+        const percentage = (mouseDelta / /*maxDelta*/  2000) * -100;
+
+        const nextPercentageUnconstrained = parseFloat(movedPercentage) + percentage;
+        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+        prevPercentage = nextPercentage;
+
+        track.animate({
+            transform: `translate(${nextPercentage}%, -50%)`
+        }, {duration: 1000, fill: "forwards"});
+
+
+/*
+    Promise.all(track.getAnimations().map((animation) => animation.finished)).then(
+            () => {
+                        for (image of images){
+                        let elRect    =   image.getBoundingClientRect();
+                        let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
+                        let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
+                        image.animate({
+                            objectPosition: `${posicion}% center`
+                        }, {duration: 300, fill: "forwards"}); }
+                }         
+        );*/
+    }
+    function animateImages(){
+        for (image of images){
+                let elRect    =   image.getBoundingClientRect();
+                let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
+                let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
+                image.animate({objectPosition: `${posicion}% center`}, {duration: 1200, fill: "forwards"})
+                                }
+                window.requestAnimationFrame(animateImages)
+
+                            }
+    
+    window.requestAnimationFrame(animateImages)
+
+
+})
