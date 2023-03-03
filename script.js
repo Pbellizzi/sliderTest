5b8c7b1483f792b26f795f19153a8fc2ff78b91f
Pablo Bellizzi <pbellizzi2@gmail.com>
2023-03-02 21:25:18 -0300

    I think i fixed the jank

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    const cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+    let request
+
+    const audio = document.querySelector("audio");
+    let track = document.querySelector("#track");
+    let images = [...document.querySelectorAll('.image')]
+
+    let mouseDownAt     = 1; 
+    let movedPercentage = 0; 
+    let prevPercentage  = 0;
+    let nextPercentageOut = 0
+
+    window.addEventListener("mousedown" ,(e) => handleOnDown(e));
+    window.addEventListener("mouseup"   ,(e) => handleOnUp(e)  );
+    window.addEventListener("mousemove" ,(e) => handleOnMove(e));
+    /*window.ontouchstart = e => handleOnDown(e.touches[0]);
+    window.ontouchend = e => handleOnUp(e.touches[0]);
+    window.ontouchmove = e => handleOnMove(e.touches[0]);*/
+
+    const handleOnDown = e => {
+          mouseDownAt = parseInt(e.clientX)
+        }
+
+    const handleOnUp = () => {
+        
+            audio.pause();
+            mouseDownAt = 0;
+            movedPercentage = prevPercentage
+        }
+
+    const handleOnMove = e => {
+         if((mouseDownAt) === 0) return
+        const mouseDelta = parseFloat(mouseDownAt) - e.clientX;    
+        /*No estoy usando el ancho de la pantalla para determinar la velocidad del scroll*/
+        /*const   maxDelta = window.innerWidth / 2;*/ 
+        const percentage = (mouseDelta / 2000) * -100;
+        const nextPercentageUnconstrained = parseFloat(movedPercentage) + percentage;
+        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        prevPercentage = nextPercentage;
+        /*track.animate({            transform: `translate3d(${nextPercentage}%, -50%, 0)`
+        }, {duration: 1000, fill: "forwards"});*/
+        /*nextPercentageOut = nextPercentage*/
+    }
+
+
+    /* Puse ambas animaciones  en la misma funcion. Queda revisar si puedo sacar el for loop de la animacion*/
+
+    function animateImages(){
+        for (image of images){
+            let elRect    =   image.getBoundingClientRect();
+            let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
+            let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
+            image.animate({objectPosition: `${posicion}% center`}, {duration: 1200, fill: "forwards"})
+                                }
+                                track.animate({transform: `translate3d(${prevPercentage}%, -50%, 0)`},{duration: 1000, fill: "forwards"});
+                                request = requestAnimationFrame(function(){animateImages()})
+
+                            }
+       
+    request = requestAnimationFrame(function(){animateImages()})
+    console.log(request)       
+})
+
+/*Ideas*/
+
     /*document.addEventListener( 'visibilitychange' , function() {
         if (document.hidden) {
             console.log('bye');
@@ -10,78 +78,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }, false );*/
 
-
-    const audio = document.querySelector("audio");
-    let track = document.querySelector("#track");
-    let images = [...document.querySelectorAll('.image')]
-
-    let mouseDownAt = 0; 
-    let movedPercentage = 0; 
-    let prevPercentage = 0;
-
-    window.addEventListener("mousedown" ,(e) => handleOnDown(e));
-    window.addEventListener("mousemove" ,(e) => handleOnMove(e));
-    window.addEventListener("mouseup"   ,(e) => handleOnUp(e)  );
-
-    /*window.ontouchstart = e => handleOnDown(e.touches[0]);
-    window.ontouchend = e => handleOnUp(e.touches[0]);
-    window.ontouchmove = e => handleOnMove(e.touches[0]);*/
-
-    
-    for (image of images){
-        let elRect    =   image.getBoundingClientRect();
-        let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
-        let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
-        /*image.style.objectPosition = `${posicion}% center`*/
-        image.setAttribute = ("objectPosition",`${posicion}% center`)
-                        }
-    
-    function animateImages(){
-        for (image of images){
-                let elRect    =   image.getBoundingClientRect();
-                let posicionUnconstrained  =  (elRect.left + elRect.width /2) / window.innerWidth * 100
-                let posicion = Math.max(Math.min(posicionUnconstrained, 100), 0)
-                image.animate({objectPosition: `${posicion}% center`}, {duration: 1200, fill: "forwards"})
-                                }
-                window.requestAnimationFrame(animateImages)
-
-                            }
-
-
-    const handleOnDown = e => {
-        mouseDownAt = parseInt(e.clientX)
-        }
-
-    const handleOnUp = () => {
-            audio.pause();
-             mouseDownAt = 0;
-            movedPercentage = prevPercentage
-        }
-    
- 
-    const handleOnMove = e => {
-         if((mouseDownAt) === 0) return
-
-
-        const mouseDelta = parseFloat(mouseDownAt) - e.clientX; 
-    
-        /*No estoy usando el ancho de la pantalla para determinar la velocidad del scroll*/
-        /*const   maxDelta = window.innerWidth / 2;*/ 
-     
-        const percentage = (mouseDelta / 2000) * -100;
-
-        const nextPercentageUnconstrained = parseFloat(movedPercentage) + percentage;
-        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-        prevPercentage = nextPercentage;
-
-        track.animate({
-            /*transform: `translate(${nextPercentage}%, -50%)`*/
-            transform: `translate3d(${nextPercentage}%, -50%, 0)`
-
-        }, {duration: 1000, fill: "forwards"});
-    }
-
 /*
     Promise.all(track.getAnimations().map((animation) => animation.finished)).then(
             () => {
@@ -92,11 +88,4 @@ document.addEventListener("DOMContentLoaded", () => {
                         image.animate({
                             objectPosition: `${posicion}% center`
                         }, {duration: 300, fill: "forwards"}); }
-                }         
-        );*/
-
-
-
-    window.requestAnimationFrame(animateImages)
-                   
-})
+               ;*/
